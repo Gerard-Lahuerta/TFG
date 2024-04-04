e94413b9fcc173ff6dc1852b988dce4a3dd84adf
Gerard-Lahuerta <gerard.lahuerta.martin@gmail.com>
2024-04-04 19:11:02 +0100

    inclusion de omp :c

--- code/src/definitions.h
+++ code/src/definitions.h
@@ -1,10 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <omp.h>
 
 #define INPUT_SIZE 1
-#define HIDDEN_SIZE 3
+#define HIDDEN_SIZE 5
 #define OUTPUT_SIZE 1
-#define N_HIDDEN 2
+#define N_HIDDEN 5
 #define LEARNING_RATE 0.1
 #define EPOCHS 1000
