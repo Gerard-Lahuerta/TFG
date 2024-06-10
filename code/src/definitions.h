c2401b8b46ceb73a8cad791ad48ba06dd4a9026c
Gerard-Lahuerta <gerard.lahuerta.martin@gmail.com>
2024-06-10 14:03:35 +0100

    aux commit

--- code/src/definitions.h
+++ code/src/definitions.h
@@ -6,11 +6,11 @@
 #define LOG_ENABLED 1
 
 #define INPUT_SIZE 1
-#define HIDDEN_SIZE 4
+#define HIDDEN_SIZE 2
 #define OUTPUT_SIZE 1
 #define N_HIDDEN 2
 #define LEARNING_RATE 0.1
-#define EPOCHS 200
+#define EPOCHS 100
 
 typedef double (*CostFunction)(double expected_output, double actual_output);
 typedef double (*ActivationFunction)(double value);
